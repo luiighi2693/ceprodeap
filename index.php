1fbca821d775f1567e05346d97c2a9d64804dada
luiighi2693 <luiscarvajal2693@gmail.com>
2016-11-16 22:48:40 -0400

    refactorizacion

--- index.php
+++ index.php
@@ -19,29 +19,25 @@
             <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#main-navbar"
                     aria-expanded="false"><span class="sr-only">Toggle navigation</span><span
                     class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button>
-            <a class="" href="index.php"><img src="images/logo_1.png" class="img-responsive" id="logo1" alt="logo"></a>
+            <a class="" onclick="$('#content').load('main.php');" href="#"><img src="images/logo_1.png" class="img-responsive" id="logo1" alt="logo"></a>
         </div>
         <div class="collapse navbar-collapse" id="main-navbar">
             <ul class="nav navbar-nav">
-                <li><a href="index.php">Inicio</a></li>
+                <li><a onclick="$('#content').load('main.php');" href="#">Inicio</a></li>
                 <li><a onclick="$('#content').load('empresa.html');" href="#">La Empresa</a></li>
-                <li><a href="servicios.html">Servicios</a></li>
-                <li><a href="clientes.html">Clientes</a></li>
+                <li><a onclick="$('#content').load('servicios.html');" href="#">Servicios</a></li>
+                <li><a onclick="$('#content').load('clientes.html');" href="#">Clientes</a></li>
                 <li><a onclick="goToEvents();" href="#">Capacitaciones</a></li>
                 <li><a  href="#">Novedades</a></li>
-                <li><a href="contacto.html">Contáctanos</a></li>
+                <li><a onclick="$('#content').load('contacto.html');" href="#">Contáctanos</a></li>
             </ul>
-            <ul class="nav navbar-nav navbar-right">
-                <div class="social-links">
-                    <ul class="list-inline">
-                        <li><a href="https://twitter.com/ceprodeap" target="_blank"><i class="fa fa-twitter fa-2x"></i></a>
-                        </li>
-                        <li><a href="https://m.facebook.com/Ceprodeap/" target="_blank"><i
-                                class="fa fa-facebook-square fa-2x"></i></a></li>
-                        <li><a href="https://www.instagram.com/ceprodeapvenezuela/" target="_blank"><i
-                                class="fa fa-instagram fa-2x"></i></a></li>
-                    </ul>
-                </div>
+            <ul class="nav navbar-nav navbar-right list-inline">
+                <li><a href="https://twitter.com/ceprodeap" target="_blank"><i class="fa fa-twitter fa-2x"></i></a>
+                </li>
+                <li><a href="https://m.facebook.com/Ceprodeap/" target="_blank"><i
+                            class="fa fa-facebook-square fa-2x"></i></a></li>
+                <li><a href="https://www.instagram.com/ceprodeapvenezuela/" target="_blank"><i
+                            class="fa fa-instagram fa-2x"></i></a></li>
             </ul>
         </div>
     </div>
@@ -54,11 +50,13 @@
             <div class="container">
                 <div class="row">
                     <div class="col-md-7">
-                        <a href="#">Inicio</a> |
-                        <a href="#">La Empresa</a> |
-                        <a href="#">Servicios</a> |
-                        <a href="#">Eventos</a> |
-                        <a href="#">Contáctanos</a>
+                        <a onclick="$('#content').load('main.php');" href="#">Inicio</a> |
+                        <a onclick="$('#content').load('empresa.html');" href="#">La Empresa</a> |
+                        <a onclick="$('#content').load('servicios.html');" href="#">Servicios</a> |
+                        <a onclick="$('#content').load('clientes.html');" href="#">Clientes</a> |
+                        <a onclick="goToEvents();" href="#">Capacitaciones</a> |
+                        <a  href="#">Novedades</a> |
+                        <a onclick="$('#content').load('contacto.html');" href="#">Contáctanos</a>
                     </div>
                     <div class="col-md-5 text-right">
                         <p class="small">Copyright &copy; 2016 - Desarrollado por L.R y J.D - Todos los derechos
@@ -74,11 +72,9 @@
 
 <script src="js/jquery.min.js"></script>
 <script src='js/bootstrap.min.js'></script>
-<script type="text/javascript"
-        src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC6eiGpyMn6DwgOn2IZFKlLMqVsF81rBvA"></script>
+<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC6eiGpyMn6DwgOn2IZFKlLMqVsF81rBvA"></script>
 <script>
     $( document ).ready(function() {
-        google.maps.event.addDomListener(window, 'load', null);
         $('#content').load('main.php');
     });
 
