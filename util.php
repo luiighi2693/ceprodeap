4c776707d6ac80b61fef13264392f59a5132cf75
luiighi2693 <luiscarvajal2693@gmail.com>
2016-11-18 21:05:08 -0400

    detalles de usuarios filtrados

--- util.php
+++ util.php
@@ -10,6 +10,14 @@ if($_POST['method']=='search'){
     search();
 }
 
+if($_POST['method']=='detailPerson'){
+    detailPerson();
+}
+
+if($_POST['method']=='activitiesByPerson'){
+    activitiesByPerson();
+}
+
 function search(){
     $mysqli = new mysqli("localhost", "root", "", "ceprodeap");
     if(count(explode(' ',$_POST['search']))==1){
@@ -50,3 +58,42 @@ function search(){
     echo json_encode($array);
 }
 
+function detailPerson(){
+    $mysqli = new mysqli("localhost", "root", "", "ceprodeap");
+    $data = $_POST['id'];
+    $query = 'SELECT * FROM usuarios WHERE id ='.$data;
+
+    $result = $mysqli->query($query);
+
+    $array = array();
+
+    for ($i = 0; $i < $result->num_rows; $i++) {
+        array_push($array,$result->fetch_array(MYSQLI_ASSOC));
+    }
+
+    mysqli_free_result($result);
+    $mysqli->close();
+
+    echo json_encode($array);
+}
+
+function activitiesByPerson(){
+    $mysqli = new mysqli("localhost", "root", "", "ceprodeap");
+    $data = $_POST['id'];
+    $query = 'SELECT DISTINCT actividades.* FROM usuarios, actividades, usuario_has_actividad 
+      WHERE usuario_has_actividad.id_usuario = '.$data.'
+        AND usuario_has_actividad.id_actividad = actividades.id';
+
+    $result = $mysqli->query($query);
+
+    $array = array();
+
+    for ($i = 0; $i < $result->num_rows; $i++) {
+        array_push($array,$result->fetch_array(MYSQLI_ASSOC));
+    }
+
+    mysqli_free_result($result);
+    $mysqli->close();
+
+    echo json_encode($array);
+}
